25311aaebb5f607fbee423d95a27ac4176389cbd
hyperfraise <damien.menigaux@polytechnique.edu>
2020-06-03 14:45:53 +0200

    remove commented lines and useless ";" as well as debug names

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 github_username="$1"
 # Update the packet manager
-#sudo apt-get update -y && sudo apt-get upgrade -y;
+sudo apt-get update -y && sudo apt-get upgrade -y
 # Install ssh
-sudo apt-get install openssh-client && sudo apt-get -y install openssh-server;
+sudo apt-get install openssh-client && sudo apt-get -y install openssh-server
 #Install curl
-sudo apt-get -y install curl;
+sudo apt-get -y install curl
 #Create ssh key for user veesion
-ssh-keygen -b 4096 -f /home/veesion/id_rsa2 -q -N "";
+ssh-keygen -b 4096 -f /home/veesion/.ssh/id_rsa -q -N ""
 #Put your ssh key in the authorized_keys
-url="https://github.com/"$github_username".keys";
-echo $url;
-curl $url >> ak;
+url="https://github.com/"$github_username".keys"
+echo $url
+curl $url >> .ssh/authorized_keys
