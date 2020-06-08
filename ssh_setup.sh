869cb77c382b6bd3d04752f44b1195e003e981aa
hyperfraise <damien.menigaux@polytechnique.edu>
2020-06-08 18:38:20 +0200

    add .ssh directory creation

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -5,7 +5,7 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 # Install ssh
 sudo apt-get install openssh-client && sudo apt-get -y install openssh-server
 #Install curl
-sudo apt-get -y install curl
+mkdir .ssh
 #Create ssh key for user veesion
 ssh-keygen -b 4096 -f /home/veesion/.ssh/id_rsa -q -N ""
 #Put your ssh key in the authorized_keys
