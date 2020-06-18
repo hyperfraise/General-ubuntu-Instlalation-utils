93b0b4ea595e0d331154cd415fead2c32f64c39c
hyperfraise <damien.menigaux@polytechnique.edu>
2020-06-18 10:00:13 +0200

    remove automatic upgrades

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 github_username="$1"
-# Update the packet manager
-sudo apt-get update -y && sudo apt-get upgrade -y
 # Install ssh
 sudo apt-get install openssh-client && sudo apt-get -y install openssh-server
 #Install curl
